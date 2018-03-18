c968ebb5ac2ae9ed61235c4e3dbbb10e2a2787b7
dhruvwa16 <league@WTS-IM-6.attlocal.net>
2018-03-17 17:28:12 -0700

    snake, game object, gamepanel

---
+++ src/GamePanel.java
@@ -0,0 +1,47 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+Timer timer;
+GamePanel(){
+	timer = new Timer(1000/60, this);
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	repaint();
+}
+void StartGame() {
+	timer.start();
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+
+public void paintComponent(Graphics g){
+	 g.fillRect(10, 10, 30, 30);
+                
+
+        }
+
+
+}
---
+++ src/Snake.java
@@ -0,0 +1,31 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+
+
+public class Snake {
+	JFrame frame;
+	public static final int width = 600;
+	public static final int height = 600;
+	GamePanel gp;
+public static void main(String[] args) {
+new Snake().setup();
+
+}
+Snake(){
+frame = new JFrame();
+frame.setSize(width, height);
+gp = new GamePanel();
+
+}
+void setup() {
+	frame.add(gp);
+	frame.getContentPane().setPreferredSize(new Dimension(width, height));
+    frame.pack();
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	gp.StartGame();
+	frame.addKeyListener(gp);
+}
+}
